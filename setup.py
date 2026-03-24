89faa8e1d2b339af3aab1b3369c7dc47050ecee8
Hannah Jones <hannah.18.jones@kcl.ac.uk>
2026-03-24 12:09:32 +0000

    Setup.py typo

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ USE_CYTHON = True
 
 CC = environ.get('CC', 'gcc')
 os.environ['CC'] = CC
-LDSHARED = environ.get('LDSHARED', CC + ' - shared')
+LDSHARED = environ.get('LDSHARED', CC + ' -shared')
 os.environ['LDSHARED'] = LDSHARED
 PREFIX = environ.get("PREFIX", '.')
 HTSLIB_INCLUDE_DIR = environ.get("HTSLIB_INCLUDE_DIR", None)
